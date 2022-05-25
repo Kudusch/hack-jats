2fa5240adf2673ac33efa081e63898762c4f397c
chainsawriot <chainsawtiney@gmail.com>
2022-05-25 22:39:37 +0200

    House keeping

--- xml/jats_injector.py
+++ xml/jats_injector.py
@@ -50,53 +50,43 @@ def gen_author_node(last_name, given_name, orcid, affiliation, aff_dict):
     sup.text = str(aff_info[0])
     return(x)
 
-aff_dict = gen_affiliation_dict(authors)
+def gen_front_node(authors, journal, doi, title, abstract):
+    aff_dict = gen_affiliation_dict(authors)
+    front_node = ET.Element("front")
+    # front > journal meta
+    journalmeta_node = ET.SubElement(front_node, "journal-meta")
+    # populate Journal Title
 
+    jid_nlmta_node = ET.SubElement(journalmeta_node, "journal-id", attrib = {"journal-id-type": "nlm-ta"})
+    jid_nlmta_node.text = journal
+    journaltitlegroup_node = ET.SubElement(journalmeta_node, "journal-title-group")
+    journaltitle_node = ET.SubElement(journaltitlegroup_node, "journal-title")
+    journaltitle_node.text = journal
+    # front > article_meta
+    articlemeta_node  = ET.SubElement(front_node, "article-meta")
+    # DOI: front > article_meta > article-id
+    aid_node = ET.SubElement(articlemeta_node, "article-id", attrib = {"pub-id-type": "doi"})
+    aid_node.text = doi
+    # title: front > article_meta > title-group > article-title
+    title_group_node = ET.SubElement(articlemeta_node, "title-group")
+    article_title_node = ET.SubElement(title_group_node, "article-title")
+    article_title_node.text = title
+    # authors: front > article_meta > contrib-group
+    contrib_group_node = ET.SubElement(articlemeta_node, "contrib-group")
+    for author in authors:
+        contrib_group_node.append(gen_author_node(author["last_name"], author["given_name"], author["orcid"], author["affiliation"], aff_dict))
+    for aff_key in aff_dict:
+        aff_info = aff_dict[aff_key]
+        aff_node = ET.SubElement(articlemeta_node, "aff", {"id": aff_info[1]})
+        label_node = ET.SubElement(aff_node, "label")
+        label_node.text = str(aff_info[0])
+        addr_node = ET.SubElement(aff_node, "addr-line")
+        addr_node.text = aff_key
+    abstract_node = ET.SubElement(articlemeta_node, "abstract")
+    _ = ET.SubElement(abstract_node, "p")
+    _.text = abstract
+    return(front_node)
+    
 root_node = ET.Element("article",attrib = {"article-type": "research-article", "dtd-version": "1.1d3", "xml:lang": "en"})
-
-# Front matter
-front_node = ET.SubElement(root_node, "front")
-
-# front > journal meta
-journalmeta_node = ET.SubElement(front_node, "journal-meta")
-
-# populate Journal Title
-
-jid_nlmta_node = ET.SubElement(journalmeta_node, "journal-id", attrib = {"journal-id-type": "nlm-ta"})
-jid_nlmta_node.text = journal
-journaltitlegroup_node = ET.SubElement(journalmeta_node, "journal-title-group")
-journaltitle_node = ET.SubElement(journaltitlegroup_node, "journal-title")
-journaltitle_node.text = journal
-
-# front > article_meta
-
-articlemeta_node  = ET.SubElement(front_node, "article-meta")
-
-# DOI: front > article_meta > article-id
-
-aid_node = ET.SubElement(articlemeta_node, "article-id", attrib = {"pub-id-type": "doi"})
-aid_node.text = doi
-
-# title: front > article_meta > title-group > article-title
-title_group_node = ET.SubElement(articlemeta_node, "title-group")
-article_title_node = ET.SubElement(title_group_node, "article-title")
-article_title_node.text = title
-
-# authors: front > article_meta > contrib-group
-contrib_group_node = ET.SubElement(articlemeta_node, "contrib-group")
-
-for author in authors:
-    contrib_group_node.append(gen_author_node(author["last_name"], author["given_name"], author["orcid"], author["affiliation"], aff_dict))
-
-for aff_key in aff_dict:
-    aff_info = aff_dict[aff_key]
-    aff_node = ET.SubElement(articlemeta_node, "aff", {"id": aff_info[1]})
-    label_node = ET.SubElement(aff_node, "label")
-    label_node.text = str(aff_info[0])
-    addr_node = ET.SubElement(aff_node, "addr-line")
-    addr_node.text = aff_key
-
-abstract_node = ET.SubElement(articlemeta_node, "abstract")
-_ = ET.SubElement(abstract_node, "p")
-_.text = abstract
-
+front_node = gen_front_node(authors, journal, doi, title, abstract)
+root_node.append(front_node)
